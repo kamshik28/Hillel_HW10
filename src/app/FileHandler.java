21d16d7b5785a1e657487175e2041c309665af0a
kamshik28 <dmytriikaminskyi@gmail.com>
2024-11-07 16:19:03 +0100

    Last commit

--- src/app/FileHandler.java
+++ src/app/FileHandler.java
@@ -5,28 +5,27 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileHandler {
-
     private final static String BASE_PATH = "files/";
 
-    public String writeFile(String fileContent) {
-        try(FileWriter fw = fileName + ".txt") {
-            fw(fileContent);
+    public String writeFile(String fileName, String fileContent) {
+        try (FileWriter fw = new FileWriter(BASE_PATH + fileName + ".txt")) {
+            fw.write(fileContent);
             return "Success.";
         } catch (Exception e) {
-            return
+            return "Failed: " + e.getMessage();
         }
     }
 
     public String readFile(String path) {
-        try (FileReader reader = new FileReader()) {
+        try (FileReader reader = new FileReader(path)) {
             int sym;
-            new StringBuilder();
+            StringBuilder stringBuilder = new StringBuilder();
             while ((sym = reader.read()) != -1) {
                 stringBuilder.append((char) sym);
             }
-            return
+            return stringBuilder.toString();
         } catch (IOException ex) {
-            return ex.getMessage();
+            return "Error: " + ex.getMessage();
         }
     }
 }
